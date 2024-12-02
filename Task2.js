daa86f9a0c36a7157329e48e9a7c64853c0e31e7
bshara1996 <bshara.karkaby@gmail.com>
2024-12-02 22:02:18 +0200

    Add files via upload

---
+++ Task2.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * Function check if the number is prime.
+ *
+ * @param {number} num - Number To Check.
+ * @returns {boolean} - Returns `true` if the number is prime - otherwise returns `false`.
+ */
+function isPrime(num) {
+    if (num < 2) // Numbers smaller that 2 are not prime
+        return false;
+    for (let i = 2; i < num; i++)
+        if (num % i === 0)
+            return false; // If number divides without a remainder, it is not prime.
+    return true;
+}
+
+
+for (let num = 0; num < 237; num++) {
+    if (isPrime(num))
+        console.log(num);
+}
